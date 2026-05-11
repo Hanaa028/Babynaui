805acda2e671da4e1bd71b2f4d4856aab374619c
Hanaa028 <chutinan280252@gmail.com>
2026-05-11 22:50:06 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "ผู้ช่วยพิ้นวย ยินดีต้อนรับคับ ♥️",
         // Activity type number (0 = Playing).
         type: 0, 
       },
@@ -88,7 +88,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
+      primary: "#ef93bd", 
       secondary: "#2F3136", 
 
       // Standard status colors for success/error/warning/info messages.
